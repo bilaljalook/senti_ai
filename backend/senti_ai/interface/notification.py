85f1630853167cbfb18bb879fe3cb0613bd9b3ae
ahmet cakan <ahmetcakan@gmx.net>
2025-03-13 05:02:30 +0100

    another change to notification method

--- backend/senti_ai/interface/notification.py
+++ backend/senti_ai/interface/notification.py
@@ -1,10 +1,13 @@
 import pushover, requests
 from senti_ai.params import *
 
-def send_pushover_notification(message, title=None, priority=1):
-
-    user_key=AHMET_USER_KEY
+def send_pushover_notification(
+    message,
+    title=None,
+    priority=1,
+    user_key=AHMET_USER_KEY,
     api_token=PUSHOVER_API_TOKEN
+    ):
 
     payload = {
         "token": api_token,
