83f1ef0b0130c6f700a4cce04181760f8c2ffbc0
Bilal Jalouk <bilaljalook@hotmail.com>
2025-03-13 17:34:34 +0100

    Mlflow, requirements, model

--- backend/senti_ai/ml_logic/registry.py
+++ backend/senti_ai/ml_logic/registry.py
@@ -1,5 +1,17 @@
+import mlflow
+from senti_ai.params import *
+import mlflow.sklearn
+from mlflow.tracking import MlflowClient
+
+
 def load_model():
-    return None
+    model_uri = f"bitcoin_price_prediction_model_test.h5"
+    model = mlflow.sklearn.load_model(model_uri)
+    mlflow.tensorflow.log_model(model=model,
+                        artifact_path="model",
+                        registered_model_name=MLFLOW_MODEL_NAME,
+                        )
+    return model
 
 def save_model():
     return None
