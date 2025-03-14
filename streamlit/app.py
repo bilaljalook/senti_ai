987b5403821102a75d6c251082af7597efda48c1
KseniaKomarova <kk.heumueller@gmail.com>
2025-03-14 09:56:04 +0100

    My yesterday last changes

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,18 +1,25 @@
 import streamlit as st
 import requests
 import streamlit.components.v1 as components
+#from app2 import load_historical_data_api
+#from app2 import load_prediction_data_api
+#from app2 import plot_market_chart
+#from app2 import plot
+from charts import load_all_data, plot_combined_chart, plot_prediction_chart
 
 import numpy as np
 import pandas as pd
 import datetime
 
+st.set_page_config(layout='wide')
+
 #variables
 url = "http://web.senti-ai.net:5367/get_historical?gcp_project=supple-folder-448412-n9&bq_dataset=sentiai&table=raw"
-chart_url='https://g.co/finance/BTC-EUR'
+bitcoin_chart_url="https://s3.tradingview.com/tv.js"
 logo_large='/Users/kk/code/logo.png'
+all_data = load_all_data()
 
 #layout
-st.set_page_config(layout='wide')
 
 st.title('Historical overview')
 
@@ -22,11 +29,34 @@ col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     with st.container(border=True, height=400):
-        st.write("Bitcoin Historical Data")
+        st.subheader("Bitcoin Historical Data", divider='blue')
         #bitcoin_chart = pd.DataFrame(xxx)
         #st.line_chart(bitcoin_chart)
         #st.image("https://g.co/finance/BTC-EUR", width=200)
-        components.iframe(chart_url, width=300, height=200)
+        components.html(
+        """
+        <div class="tradingview-widget-container">
+            <div id="tradingview_74048"></div>
+            <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+            <script type="text/javascript">
+                new TradingView.widget({
+                    "autosize": true,
+                    "symbol": "COINBASE:BTCEUR",
+                    "interval": "D",
+                    "timezone": "Europe/Zurich",
+                    "theme": "light",
+                    "style": "1",
+                    "locale": "en",
+                    "toolbar_bg": "#f1f3f6",
+                    "enable_publishing": false,
+                    "hide_side_toolbar": false,
+                    "container_id": "tradingview_74048"
+                });
+            </script>
+        </div>
+        """,
+        height=200,
+    )
 
 with col2:
     with st.container(border=True, height=400):
@@ -52,6 +82,8 @@ with col4:
 
 with st.container(border=True, height=500):
     st.subheader('Overlapping charts', divider=True)
+    #combined_chart=plot_combined_chart(all_data)
+    #st.plotly_chart(combined_chart, use_container_width=True)
     #nasdaq_chart = pd.DataFrame(xxx)
     #st.line_chart(nasdaq_chart)
     st.slider(label='days', min_value=1, max_value=365, step=1)
